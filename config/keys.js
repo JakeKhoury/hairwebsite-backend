515e24fe80230db83db9d63ff734f871e2b1f051
JakeKhoury <1.jacob.khoury@gmail.com>
2019-12-02 19:11:04 -0600

    first commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://jakekhoury:123@cluster0-5rqtj.gcp.mongodb.net/test?retryWrites=true&w=majority",
+    secret: "secret"
+};
---
+++ src/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const passport = require("passport");
+const app = express();
+const users = require('../routes/user');
+const mongoose = require('mongoose');
+
+const index = require('../routes/index');
+const api = require('../routes/api/index');
+
+// DB Config
+const db = require("../config/keys").mongoURI;
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+require("../config/passport")(passport);
+
+// Middlware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(passport.initialize());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ health: "okay" });
+});
+
+
+
+// Routes
+app.use("/user", users);
+
+app.use('/', index);
+app.use('/api', api);
+
+app.listen(8080, () => {
+  console.log("Listening on 8080");
+});
