15a47cef6e77b025279f9d88363bd21afe03cf53
Donny <donny@avyxia.com>
2019-12-03 21:20:50 -0600

    Stuff

--- src/server.js
+++ src/server.js
@@ -3,14 +3,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const passport = require("passport");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // login routes
-const users = require('../routes/user');
-
-// appointment routes
-const index = require('../routes/index');
-const api = require('../routes/api/index');
+const users = require("../routes/user");
 
 // DB Config
 const db = require("../config/keys").mongoURI;
@@ -24,23 +20,20 @@ require("../config/passport")(passport);
 
 // Middlware
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(passport.initialize());
 
 app.get("/health", (req, res) => {
   res.status(200).json({ health: "okay" });
 });
 
-
-
 // Routes
 // login routes
 app.use("/user", users);
 
-// appointment routes
-app.use('/', index);
-app.use('/api', api);
+const bookings = require("../routes/booking");
+app.use("/booking", bookings);
 
 app.listen(8080, () => {
   console.log("Listening on 8080");
